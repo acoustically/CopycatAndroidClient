eb330c96975ba07f9e068e7393330c9d3d195c86
acoustically <acoustically@kookmin.ac.kr>
2016-11-18 19:19:30 +0900

    get permission bluetooth,
     if bluetooth is off then pop up on page

--- app/src/main/java/kmu/mobile/programming/smartmouse/MainActivity.java
+++ app/src/main/java/kmu/mobile/programming/smartmouse/MainActivity.java
@@ -1,13 +1,41 @@
 package kmu.mobile.programming.smartmouse;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import kmu.mobile.programming.smartmouse.comunication.bluetooth.Bluetooth;
 
 public class MainActivity extends AppCompatActivity {
+  public static int BLUETOOTH_ON = 0001;
+  private Bluetooth mBluetooth;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    getPermission();
+    mBluetooth = Bluetooth.getInstance(this);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == BLUETOOTH_ON) {
+      if (resultCode == RESULT_OK) {
+        Log.d("MYLOG", "bluetooth on");
+        mBluetooth.isOn = true;
+      } else {
+        Log.d("MYLOG", "bluetooth off");
+      }
     }
+  }
+
+  private void getPermission() {
+    int permission = checkSelfPermission(Manifest.permission.BLUETOOTH);
+    if(permission == PackageManager.PERMISSION_DENIED) {
+      requestPermissions(new String[]{Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN}, 1);
+    }
+  }
 }
---
+++ app/src/main/java/kmu/mobile/programming/smartmouse/comunication/Communication.java
@@ -0,0 +1,9 @@
+package kmu.mobile.programming.smartmouse.comunication;
+
+/**
+ * Created by acoustically on 16. 11. 18.
+ */
+public interface Communication {
+  void send();
+  void receive();
+}
